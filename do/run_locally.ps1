db9e5047f0a0d0837f92c931d373288826a102b8
Drewroen <drewroen@gmail.com>
2020-08-03 18:08:41 -0500

    Update gamemap for upcoming blocks

--- do/run_locally.ps1
+++ do/run_locally.ps1
@@ -1,5 +1,5 @@
 tsc
-mkdir ./dist/objects/levels
+mkdir -p ./dist/objects/levels
 Copy-Item ./objects/levels/CHIPS_MMO.dat ./dist/objects/levels/CHIPS_MMO.dat
 ng build
 nodemon dist/server/index.js
