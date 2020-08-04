1248dce4bdcc44e3823e9d47a38c48b86b37d832
Drewroen <drewroen@gmail.com>
2020-08-03 21:53:57 -0500

    Generate second layer then first layer

--- do/run_locally.ps1
+++ do/run_locally.ps1
@@ -1,5 +1,5 @@
 tsc
 mkdir -p ./dist/objects/levels
-Copy-Item ./objects/levels/CHIPS_MMO.dat ./dist/objects/levels/CHIPS_MMO.dat
+cp ./objects/levels/CHIPS_MMO.dat ./dist/objects/levels/CHIPS_MMO.dat
 ng build
 nodemon dist/server/index.js
--- objects/gameMap.ts
+++ objects/gameMap.ts
@@ -77,11 +77,40 @@ export class GameMap {
       level = level.slice(firstLayerSize);
       const secondLayerSize = this.unsignedWordToInt(level.slice(0, 2));
       level = level.slice(2);
-      const secondLayerInfo: string[] = level.slice(0, secondLayerSize);
+      let secondLayerInfo: string[] = level.slice(0, secondLayerSize);
       level = level.slice(secondLayerSize);
       const remainingInfo = level;
 
       let position = 0;
+      while(secondLayerInfo.length !== 0)
+      {
+        const value = secondLayerInfo.slice(0, 1);
+        let repeatLength;
+        let blockType;
+        let cutLength;
+        if (value[0] === 'ff')
+        {
+          repeatLength = this.hexToInt(secondLayerInfo.slice(1, 2)[0]);
+          blockType = secondLayerInfo.slice(2, 3)[0];
+          cutLength = 3;
+        }
+        else
+        {
+          repeatLength = 1;
+          blockType = value[0];
+          cutLength = 1;
+        }
+        for(let i = 0; i < repeatLength; i++)
+        {
+          const x = position % Constants.MAP_SIZE;
+          const y = Math.floor(position / Constants.MAP_SIZE);
+          this.setTileFromId(x, y, blockType, mobs);
+          position++;
+        }
+        secondLayerInfo = secondLayerInfo.slice(cutLength);
+      }
+
+      position = 0;
       while(firstLayerInfo.length !== 0)
       {
         const value = firstLayerInfo.slice(0, 1);
@@ -104,175 +133,11 @@ export class GameMap {
         {
           const x = position % Constants.MAP_SIZE;
           const y = Math.floor(position / Constants.MAP_SIZE);
-          switch(blockType)
-          {
-            case '00': this.terrainTiles[x][y] = new BlankTile(); break;
-            case '01': this.terrainTiles[x][y] = new WallTile(); break;
-            case '02': this.objectTiles[x][y] = new ChipTile(); break;
-            case '03': this.terrainTiles[x][y] = new WaterTile(); break;
-            case '04': console.log('Fire'); break;
-            case '05': console.log('Invisible wall'); break;
-            case '06': console.log('block one direction'); break;
-            case '07': console.log('block one direction'); break;
-            case '08': console.log('block one direction'); break;
-            case '09': console.log('block one direction'); break;
-            case '0a': console.log('block'); break;
-            case '0b': console.log('dirt'); break;
-            case '0c': this.terrainTiles[x][y] = new IceTile(); break;
-            case '0d': this.terrainTiles[x][y] = new ForceTile(Constants.DIRECTION_DOWN); break;
-            case '0e': console.log('clone'); break;
-            case '0f': console.log('clone'); break;
-            case '10': console.log('clone'); break;
-            case '11': console.log('clone'); break;
-            case '12': this.terrainTiles[x][y] = new ForceTile(Constants.DIRECTION_UP); break;
-            case '13': this.terrainTiles[x][y] = new ForceTile(Constants.DIRECTION_RIGHT); break;
-            case '14': this.terrainTiles[x][y] = new ForceTile(Constants.DIRECTION_LEFT); break;
-            case '15': this.terrainTiles[x][y] = new FinishTile(); break;
-            case '16': console.log('key door'); break;
-            case '17': console.log('key door'); break;
-            case '18': console.log('key door'); break;
-            case '19': console.log('key door'); break;
-            case '1a': console.log('ice corner'); break;
-            case '1b': console.log('ice corner'); break;
-            case '1c': console.log('ice corner'); break;
-            case '1d': console.log('ice corner'); break;
-            case '1e': console.log('blue wall'); break;
-            case '1f': console.log('blue wall'); break;
-            case '21': console.log('thief'); break;
-            case '22': this.terrainTiles[x][y] = new SocketTile(); break;
-            case '23': console.log('green button'); break;
-            case '24': console.log('red button'); break;
-            case '25': console.log('green button block'); break;
-            case '26': console.log('green button block'); break;
-            case '27': console.log('brown button'); break;
-            case '28': console.log('blue button'); break;
-            case '29': console.log('teleport'); break;
-            case '2a': console.log('bomb'); break;
-            case '2b': console.log('trap'); break;
-            case '2c': console.log('invisible wall'); break;
-            case '2d': console.log('gravel'); break;
-            case '2e': console.log('cell block'); break;
-            case '2f': console.log('hint'); break;
-            case '30': console.log('block two directions'); break;
-            case '31': console.log('cloning machine'); break;
-            case '32': console.log('force random direction'); break;
-            case '40': this.addMob(x, y, new BugTile(Constants.DIRECTION_UP), mobs); break;
-            case '41': this.addMob(x, y, new BugTile(Constants.DIRECTION_LEFT), mobs); break;
-            case '42': this.addMob(x, y, new BugTile(Constants.DIRECTION_DOWN), mobs); break;
-            case '43': this.addMob(x, y, new BugTile(Constants.DIRECTION_RIGHT), mobs); break;
-            case '44': this.addMob(x, y, new FireballTile(Constants.DIRECTION_UP), mobs); break;
-            case '45': this.addMob(x, y, new FireballTile(Constants.DIRECTION_LEFT), mobs); break;
-            case '46': this.addMob(x, y, new FireballTile(Constants.DIRECTION_DOWN), mobs); break;
-            case '47': this.addMob(x, y, new FireballTile(Constants.DIRECTION_RIGHT), mobs); break;
-            case '48': this.addMob(x, y, new BallTile(Constants.DIRECTION_UP), mobs); break;
-            case '49': this.addMob(x, y, new BallTile(Constants.DIRECTION_LEFT), mobs); break;
-            case '4a': this.addMob(x, y, new BallTile(Constants.DIRECTION_DOWN), mobs); break;
-            case '4b': this.addMob(x, y, new BallTile(Constants.DIRECTION_RIGHT), mobs); break;
-            case '4c': console.log('tank'); break;
-            case '4d': console.log('tank'); break;
-            case '4e': console.log('tank'); break;
-            case '4f': console.log('tank'); break;
-            case '50': this.addMob(x, y, new GliderTile(Constants.DIRECTION_UP), mobs); break;
-            case '51': this.addMob(x, y, new GliderTile(Constants.DIRECTION_LEFT), mobs); break;
-            case '52': this.addMob(x, y, new GliderTile(Constants.DIRECTION_DOWN), mobs); break;
-            case '53': this.addMob(x, y, new GliderTile(Constants.DIRECTION_RIGHT), mobs); break;
-            case '54': this.addMob(x, y, new TeethTile(Constants.DIRECTION_DOWN), mobs); break;
-            case '55': this.addMob(x, y, new TeethTile(Constants.DIRECTION_DOWN), mobs); break;
-            case '56': this.addMob(x, y, new TeethTile(Constants.DIRECTION_DOWN), mobs); break;
-            case '57': this.addMob(x, y, new TeethTile(Constants.DIRECTION_DOWN), mobs); break;
-            case '58': this.addMob(x, y, new WalkerTile(Constants.DIRECTION_UP), mobs); break;
-            case '59': this.addMob(x, y, new WalkerTile(Constants.DIRECTION_LEFT), mobs); break;
-            case '5a': this.addMob(x, y, new WalkerTile(Constants.DIRECTION_DOWN), mobs); break;
-            case '5b': this.addMob(x, y, new WalkerTile(Constants.DIRECTION_RIGHT), mobs); break;
-            case '5c': this.addMob(x, y, new BlobTile(Constants.DIRECTION_UP), mobs); break;
-            case '5d': this.addMob(x, y, new BlobTile(Constants.DIRECTION_LEFT), mobs); break;
-            case '5e': this.addMob(x, y, new BlobTile(Constants.DIRECTION_DOWN), mobs); break;
-            case '5f': this.addMob(x, y, new BlobTile(Constants.DIRECTION_RIGHT), mobs); break;
-            case '60': this.addMob(x, y, new ParemeciumTile(Constants.DIRECTION_UP), mobs); break;
-            case '61': this.addMob(x, y, new ParemeciumTile(Constants.DIRECTION_LEFT), mobs); break;
-            case '62': this.addMob(x, y, new ParemeciumTile(Constants.DIRECTION_DOWN), mobs); break;
-            case '63': this.addMob(x, y, new ParemeciumTile(Constants.DIRECTION_RIGHT), mobs); break;
-            case '64': console.log('key'); break;
-            case '65': console.log('key'); break;
-            case '66': console.log('key'); break;
-            case '67': console.log('key'); break;
-            case '68': console.log('item'); break;
-            case '69': console.log('item'); break;
-            case '6a': console.log('item'); break;
-            case '6b': console.log('item'); break;
-            default: console.log('Unknown block type: ' + blockType); break;
-          }
+          this.setTileFromId(x, y, blockType, mobs);
           position++;
         }
         firstLayerInfo = firstLayerInfo.slice(cutLength);
       }
-
-      // this.terrainTiles[0][0] = new WallTile();
-      // this.terrainTiles[0][1] = new WallTile();
-      // this.terrainTiles[0][2] = new SocketTile();
-      // this.terrainTiles[0][3] = new WallTile();
-      // this.terrainTiles[0][4] = new WallTile();
-      // this.terrainTiles[4][0] = new WallTile();
-      // this.terrainTiles[4][1] = new WallTile();
-      // this.terrainTiles[4][2] = new SocketTile();
-      // this.terrainTiles[4][3] = new WallTile();
-      // this.terrainTiles[4][4] = new WallTile();
-      // this.terrainTiles[1][0] = new WallTile();
-      // this.terrainTiles[2][0] = new SocketTile();
-      // this.terrainTiles[3][0] = new WallTile();
-      // this.terrainTiles[1][4] = new WallTile();
-      // this.terrainTiles[2][4] = new SocketTile();
-      // this.terrainTiles[3][4] = new WallTile();
-
-      // this.terrainTiles[1][1] = new FinishTile();
-      // this.terrainTiles[2][1] = new FinishTile();
-      // this.terrainTiles[3][1] = new FinishTile();
-      // this.terrainTiles[1][2] = new FinishTile();
-      // this.terrainTiles[2][2] = new FinishTile();
-      // this.terrainTiles[3][2] = new FinishTile();
-      // this.terrainTiles[1][3] = new FinishTile();
-      // this.terrainTiles[2][3] = new FinishTile();
-      // this.terrainTiles[3][3] = new FinishTile();
-
-      // for(let i = 1; i <= 8; i++)
-      //   for(let j = 5; j<= 12; j++)
-      //     this.terrainTiles[i][j] = new ForceTile(Constants.DIRECTION_DOWN);
-
-      // this.addMob(15, 15, new WalkerTile(Constants.DIRECTION_LEFT), mobs);
-      // this.addMob(15, 16, new BallTile(Constants.DIRECTION_UP), mobs);
-      // this.addMob(15, 17, new FireballTile(Constants.DIRECTION_LEFT), mobs);
-      // this.addMob(15, 18, new GliderTile(Constants.DIRECTION_LEFT), mobs);
-      // this.addMob(15, 19, new BugTile(Constants.DIRECTION_DOWN), mobs);
-      // this.addMob(16, 19, new ParemeciumTile(Constants.DIRECTION_DOWN), mobs);
-      // this.addMob(7, 7, new BlobTile(Constants.DIRECTION_UP), mobs);
-      // this.addMob(7, 8, new BlobTile(Constants.DIRECTION_UP), mobs);
-      // this.addMob(7, 9, new BlobTile(Constants.DIRECTION_UP), mobs);
-      // this.addMob(7, 10, new BlobTile(Constants.DIRECTION_UP), mobs);
-      // this.addMob(6, 7, new TeethTile(Constants.DIRECTION_UP), mobs);
-      // this.addMob(6, 8, new TeethTile(Constants.DIRECTION_UP), mobs);
-      // this.addMob(6, 9, new TeethTile(Constants.DIRECTION_UP), mobs);
-      // this.addMob(6, 10, new TeethTile(Constants.DIRECTION_UP), mobs);
-
-      // this.terrainTiles[8][1] = new IceTile();
-      // this.terrainTiles[8][2] = new IceTile();
-      // this.terrainTiles[8][3] = new IceTile();
-      // this.terrainTiles[8][4] = new IceTile();
-      // this.terrainTiles[8][0] = new WallTile();
-
-      // for(let i = 10; i <= 20; i++)
-      // {
-      //   this.terrainTiles[10][i] = new WallTile();
-      //   this.terrainTiles[20][i] = new WallTile();
-      //   this.terrainTiles[i][10] = new WallTile();
-      //   this.terrainTiles[i][20] = new WallTile();
-      // }
-
-      // this.terrainTiles[10][15] = new IceTile();
-      // this.terrainTiles[20][15] = new IceTile();
-
-      // for(let i = 11; i < 14; i++)
-      //   for(let j = 11; j < 14; j++)
-      //     this.terrainTiles[i][j] = new IceTile();
     }
 
     spawnChips(): void {
@@ -325,5 +190,107 @@ export class GameMap {
     private unsignedWordToInt(data: string[]): number
     {
       return parseInt('0x' + data[0], 16) + (parseInt('0x' + data[1], 16) * (16 * 16))
+    }
+
+    private setTileFromId(x: number, y: number, blockType: string, mobs: Mob[]): void
+    {
+      switch(blockType)
+      {
+        case '00': this.terrainTiles[x][y] = new BlankTile(); break;
+        case '01': this.terrainTiles[x][y] = new WallTile(); break;
+        case '02': this.objectTiles[x][y] = new ChipTile(); break;
+        case '03': this.terrainTiles[x][y] = new WaterTile(); break;
+        case '04': console.log('Fire'); break;
+        case '05': console.log('Invisible wall'); break;
+        case '06': console.log('block one direction'); break;
+        case '07': console.log('block one direction'); break;
+        case '08': console.log('block one direction'); break;
+        case '09': console.log('block one direction'); break;
+        case '0a': console.log('block'); break;
+        case '0b': console.log('dirt'); break;
+        case '0c': this.terrainTiles[x][y] = new IceTile(); break;
+        case '0d': this.terrainTiles[x][y] = new ForceTile(Constants.DIRECTION_DOWN); break;
+        case '0e': console.log('clone'); break;
+        case '0f': console.log('clone'); break;
+        case '10': console.log('clone'); break;
+        case '11': console.log('clone'); break;
+        case '12': this.terrainTiles[x][y] = new ForceTile(Constants.DIRECTION_UP); break;
+        case '13': this.terrainTiles[x][y] = new ForceTile(Constants.DIRECTION_RIGHT); break;
+        case '14': this.terrainTiles[x][y] = new ForceTile(Constants.DIRECTION_LEFT); break;
+        case '15': this.terrainTiles[x][y] = new FinishTile(); break;
+        case '16': console.log('key door'); break;
+        case '17': console.log('key door'); break;
+        case '18': console.log('key door'); break;
+        case '19': console.log('key door'); break;
+        case '1a': console.log('ice corner'); break;
+        case '1b': console.log('ice corner'); break;
+        case '1c': console.log('ice corner'); break;
+        case '1d': console.log('ice corner'); break;
+        case '1e': console.log('blue wall'); break;
+        case '1f': console.log('blue wall'); break;
+        case '21': console.log('thief'); break;
+        case '22': this.terrainTiles[x][y] = new SocketTile(); break;
+        case '23': console.log('green button'); break;
+        case '24': console.log('red button'); break;
+        case '25': console.log('green button block'); break;
+        case '26': console.log('green button block'); break;
+        case '27': console.log('brown button'); break;
+        case '28': console.log('blue button'); break;
+        case '29': console.log('teleport'); break;
+        case '2a': console.log('bomb'); break;
+        case '2b': console.log('trap'); break;
+        case '2c': console.log('invisible wall'); break;
+        case '2d': console.log('gravel'); break;
+        case '2e': console.log('cell block'); break;
+        case '2f': console.log('hint'); break;
+        case '30': console.log('block two directions'); break;
+        case '31': console.log('cloning machine'); break;
+        case '32': console.log('force random direction'); break;
+        case '40': this.addMob(x, y, new BugTile(Constants.DIRECTION_UP), mobs); break;
+        case '41': this.addMob(x, y, new BugTile(Constants.DIRECTION_LEFT), mobs); break;
+        case '42': this.addMob(x, y, new BugTile(Constants.DIRECTION_DOWN), mobs); break;
+        case '43': this.addMob(x, y, new BugTile(Constants.DIRECTION_RIGHT), mobs); break;
+        case '44': this.addMob(x, y, new FireballTile(Constants.DIRECTION_UP), mobs); break;
+        case '45': this.addMob(x, y, new FireballTile(Constants.DIRECTION_LEFT), mobs); break;
+        case '46': this.addMob(x, y, new FireballTile(Constants.DIRECTION_DOWN), mobs); break;
+        case '47': this.addMob(x, y, new FireballTile(Constants.DIRECTION_RIGHT), mobs); break;
+        case '48': this.addMob(x, y, new BallTile(Constants.DIRECTION_UP), mobs); break;
+        case '49': this.addMob(x, y, new BallTile(Constants.DIRECTION_LEFT), mobs); break;
+        case '4a': this.addMob(x, y, new BallTile(Constants.DIRECTION_DOWN), mobs); break;
+        case '4b': this.addMob(x, y, new BallTile(Constants.DIRECTION_RIGHT), mobs); break;
+        case '4c': console.log('tank'); break;
+        case '4d': console.log('tank'); break;
+        case '4e': console.log('tank'); break;
+        case '4f': console.log('tank'); break;
+        case '50': this.addMob(x, y, new GliderTile(Constants.DIRECTION_UP), mobs); break;
+        case '51': this.addMob(x, y, new GliderTile(Constants.DIRECTION_LEFT), mobs); break;
+        case '52': this.addMob(x, y, new GliderTile(Constants.DIRECTION_DOWN), mobs); break;
+        case '53': this.addMob(x, y, new GliderTile(Constants.DIRECTION_RIGHT), mobs); break;
+        case '54': this.addMob(x, y, new TeethTile(Constants.DIRECTION_DOWN), mobs); break;
+        case '55': this.addMob(x, y, new TeethTile(Constants.DIRECTION_DOWN), mobs); break;
+        case '56': this.addMob(x, y, new TeethTile(Constants.DIRECTION_DOWN), mobs); break;
+        case '57': this.addMob(x, y, new TeethTile(Constants.DIRECTION_DOWN), mobs); break;
+        case '58': this.addMob(x, y, new WalkerTile(Constants.DIRECTION_UP), mobs); break;
+        case '59': this.addMob(x, y, new WalkerTile(Constants.DIRECTION_LEFT), mobs); break;
+        case '5a': this.addMob(x, y, new WalkerTile(Constants.DIRECTION_DOWN), mobs); break;
+        case '5b': this.addMob(x, y, new WalkerTile(Constants.DIRECTION_RIGHT), mobs); break;
+        case '5c': this.addMob(x, y, new BlobTile(Constants.DIRECTION_UP), mobs); break;
+        case '5d': this.addMob(x, y, new BlobTile(Constants.DIRECTION_LEFT), mobs); break;
+        case '5e': this.addMob(x, y, new BlobTile(Constants.DIRECTION_DOWN), mobs); break;
+        case '5f': this.addMob(x, y, new BlobTile(Constants.DIRECTION_RIGHT), mobs); break;
+        case '60': this.addMob(x, y, new ParemeciumTile(Constants.DIRECTION_UP), mobs); break;
+        case '61': this.addMob(x, y, new ParemeciumTile(Constants.DIRECTION_LEFT), mobs); break;
+        case '62': this.addMob(x, y, new ParemeciumTile(Constants.DIRECTION_DOWN), mobs); break;
+        case '63': this.addMob(x, y, new ParemeciumTile(Constants.DIRECTION_RIGHT), mobs); break;
+        case '64': console.log('key'); break;
+        case '65': console.log('key'); break;
+        case '66': console.log('key'); break;
+        case '67': console.log('key'); break;
+        case '68': console.log('item'); break;
+        case '69': console.log('item'); break;
+        case '6a': console.log('item'); break;
+        case '6b': console.log('item'); break;
+        default: console.log('Unknown block type: ' + blockType); break;
+      }
     }
 }
