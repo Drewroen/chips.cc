a2c38bf481f0a7d36a9e559d7ce8385a5c6a1c8e
Drewroen <drewroen@gmail.com>
2020-08-23 17:30:37 -0500

    Lower game fps, increase server fps

--- constants/constants.ts
+++ constants/constants.ts
@@ -2,8 +2,8 @@ export class Constants {
     public static readonly MAP_SIZE = 32;
     public static readonly MAP_VIEW_SIZE = 9;
     public static readonly TILE_SIZE = 32;
-    public static readonly GAME_FPS = 60;
-    public static readonly SOCKET_FPS = 30;
+    public static readonly GAME_FPS = 30;
+    public static readonly SOCKET_FPS = 60;
 
     public static readonly GAME_LOBBIES = 3;
     public static readonly GAME_LOBBY_MAX_SIZE = 16;
