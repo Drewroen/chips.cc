400a1bdebcbe07fe76aabfbb7fc0f2638e8bef24
Drew Roen <drewroen@gmail.com>
2022-06-08 10:09:24 -0500

    AWS

--- server/index.ts
+++ server/index.ts
@@ -12,6 +12,8 @@ import { ImageDiff } from './../static/imageDiff/imageDiff';
 
 // App setup
 AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: 'us-east-2',
 });
 
